83c016e93d407a530f6f5e9254b1b528affe5365
BlackCommando2 <keshavsoni2002@gmail.com>
2023-08-24 18:08:29 +0530

    session: combined shared thread and genarics

---
+++ HelloTest/MethodGenarics.java
@@ -0,0 +1,28 @@
+class DemoClass {
+	
+  public <T> void genericsMethod(T data) {
+    System.out.println("Generics Method:");
+    System.out.println("Data Passed: " + data);
+  }
+  
+  public <T>  void test(T data1)
+  {
+	  System.out.println(data1);
+  }
+}
+public class MethodGenarics {
+
+	public static void main(String[] args) {
+		DemoClass demo = new DemoClass();
+
+	   demo.<String> genericsMethod("hhh");
+	    demo.<String>genericsMethod("Java Programming");
+
+	  demo.<Double>genericsMethod(45.78);
+	    demo.<Integer>genericsMethod(25);
+	    
+	  
+	    
+	    demo.<Double>test(45.789);
+	  }
+	}
